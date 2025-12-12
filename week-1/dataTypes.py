1deb4aff14e68368b6139b2fb4930f646ae6b163
hemanth kumar kottakota <hemanthkottakota07@gmail.com>
2025-12-12 16:39:28 +0530

    worked on datatypes in python

--- week-1/dataTypes.py
+++ week-1/dataTypes.py
@@ -1,21 +1,12 @@
-a = 10
-b = 10.10
-c = "hello world"
-d = True
-e = "10"
-print("normal values")
-print(a,b,c,d,e)
-
-print("after converting all to integers")
-print(int(b),int(d),int(e))
-#print(int(c)) ValueError: invalid literal for int() with base 10: 'hello world'
-
-print("after converting all to strings")
-print(str(a),str(b),str(d))
-
-print("after converting all to float")
-print(float(a),float(d),float(e))
-#print(float(c)) ValueError: could not convert string to float: 'hello world'
-
-print("after coonverting all to boolean")
-print(bool(a),bool(b),bool(c),bool(d),bool(e))
+intVar = 10
+stringVar = "hemanth"
+floatVar = 3.14
+boolVar = True
+listVar = [1,2,3,"hello"]
+tupleVar = (1,"hi",3)
+print(type(intVar))
+print(type(stringVar))
+print(type(floatVar))
+print(type(boolVar))
+print(type(listVar))
+print(type(tupleVar))
